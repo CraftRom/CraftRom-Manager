b922eee44aa4edde9e09822e526ef9d2d63c1328
Galitsky Dmytro <melles.android@gmail.com>
2021-01-31 00:14:03 +0200

    Add Terminal

--- app/src/main/java/com/craftrom/manager/MainActivity.kt
+++ app/src/main/java/com/craftrom/manager/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.craftrom.manager
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,16 +27,11 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_device, R.id.nav_kernel, R.id.nav_module, R.id.nav_about), drawerLayout)
+                R.id.nav_device, R.id.nav_kernel, R.id.nav_module, R.id.nav_terminal, R.id.nav_about), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
---
+++ app/src/main/java/com/craftrom/manager/utils/Utils.java
@@ -0,0 +1,161 @@
+package com.craftrom.manager.utils;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.craftrom.manager.BuildConfig;
+import com.craftrom.manager.R;
+import com.google.android.material.snackbar.Snackbar;
+import com.topjohnwu.superuser.Shell;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/*
+ * Created by sunilpaulmathew <sunil.kde@gmail.com> on September 25, 2020
+ */
+
+public class Utils {
+
+    private static Process mProcess;
+
+    static {
+        Shell.Config.verboseLogging(BuildConfig.DEBUG);
+        Shell.Config.setTimeout(10);
+    }
+
+    public static void runCommand(String command, List<String> output) {
+        try {
+            mProcess = Runtime.getRuntime().exec(command);
+            BufferedReader mInput = new BufferedReader(new InputStreamReader(mProcess.getInputStream()));
+            BufferedReader mError = new BufferedReader(new InputStreamReader(mProcess.getErrorStream()));
+            String line;
+            while ((line = mInput.readLine()) != null) {
+                output.add(line);
+            }
+            while ((line = mError.readLine()) != null) {
+                output.add(line);
+            }
+            mProcess.waitFor();
+        } catch (Exception e) {
+            output.add(e.getMessage());
+        }
+    }
+
+    public static void runRootCommand(String command, List<String> output) {
+        Shell.su(command).to(output, output).exec();
+    }
+
+    public static boolean rootAccess() {
+        return Shell.rootAccess();
+    }
+
+    public static void closeSU() {
+        try {
+            Objects.requireNonNull(Shell.getCachedShell()).close();
+        } catch (Exception ignored) {
+        }
+    }
+
+    public static void destroyProcess() {
+        mProcess.destroy();
+    }
+
+    public static boolean isDarkTheme(Context context) {
+        int currentNightMode = context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES;
+    }
+
+    public static void initializeAppTheme(Context context) {
+        if (isDarkTheme(context)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+
+    public static void goToSettings(Activity activity) {
+        Intent settings = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        settings.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null);
+        settings.setData(uri);
+        activity.startActivity(settings);
+        activity.finish();
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert cm != null;
+        return (cm.getActiveNetworkInfo() != null) && cm.getActiveNetworkInfo().isConnectedOrConnecting();
+    }
+
+    public static void launchUrl(String url, Activity activity) {
+        if (isNetworkAvailable(activity)) {
+            try {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                activity.startActivity(i);
+            } catch (ActivityNotFoundException ignored) {
+            }
+        } else {
+            showSnackbar(activity.findViewById(android.R.id.content), activity.getString(R.string.network_unavailable));
+        }
+    }
+
+    public static void showSnackbar(View view, String message) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG);
+        snackbar.setAction(R.string.dismiss, v -> snackbar.dismiss());
+        snackbar.show();
+    }
+
+    public static String readAssetFile(Context context, String file) {
+        InputStream input = null;
+        BufferedReader buf = null;
+        try {
+            StringBuilder s = new StringBuilder();
+            input = context.getAssets().open(file);
+            buf = new BufferedReader(new InputStreamReader(input));
+
+            String str;
+            while ((str = buf.readLine()) != null) {
+                s.append(str).append("\n");
+            }
+            return s.toString().trim();
+        } catch (IOException ignored) {
+        } finally {
+            try {
+                if (input != null) input.close();
+                if (buf != null) buf.close();
+            } catch (IOException ignored) {
+            }
+        }
+        return null;
+    }
+
+    public static String getOutput(List<String> output) {
+        List<String> mData = new ArrayList<>();
+        for (String line : output.toString().substring(1, output.toString().length() - 1).replace(
+                ", ","\n").replace("ui_print","").split("\\r?\\n")) {
+            if (!line.startsWith("progress")) {
+                mData.add(line);
+            }
+        }
+        return mData.toString().substring(1, mData.toString().length() - 1).replace(", ","\n")
+                .replaceAll("(?m)^[ \t]*\r?\n", "");
+    }
+
+}
