be56d796830a9985fe9c5d44bc1fc2e486bec77b
Galitsky Dmytro <melles.android@gmail.com>
2021-02-06 00:31:24 +0200

    Add RSS Feed

--- app/src/main/java/com/craftrom/manager/fragments/device/DeviceFragment.kt
+++ app/src/main/java/com/craftrom/manager/fragments/device/DeviceFragment.kt
@@ -1,19 +1,35 @@
 package com.craftrom.manager.fragments.device
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.craftrom.manager.R
 import com.craftrom.manager.utils.Device
+import com.craftrom.manager.utils.RSS.RssItemsAdapter
+import com.craftrom.manager.utils.RSS.RssService
+import com.craftrom.manager.utils.RSS.converter.RssConverterFactory
+import com.craftrom.manager.utils.RSS.converter.RssFeed
+import com.craftrom.manager.utils.RSS.converter.RssItem
 import com.craftrom.manager.utils.root.CheckRoot
 import com.craftrom.manager.utils.root.RootUtils
 import com.craftrom.manager.utils.storage.isDiskEncrypted
+import kotlinx.android.synthetic.main.fragment_rss.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
-class DeviceFragment : Fragment() {
+class DeviceFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener,
+RssItemsAdapter.OnItemClickListener {
     private lateinit var  oem_name: TextView
     private lateinit var disk_status: TextView
     private lateinit var root_status: TextView
@@ -22,9 +38,15 @@ class DeviceFragment : Fragment() {
     private lateinit var rom: TextView
     private lateinit var android_version: TextView
     private lateinit var fingerprint: TextView
-
+    private var feedUrl: String? = null
+    private lateinit var mAdapter: RssItemsAdapter
     var root: View? = null
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        feedUrl = arguments?.getString(DeviceFragment.KEY_FEED)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -39,6 +61,7 @@ class DeviceFragment : Fragment() {
         rom = root.findViewById(R.id.rom)
         android_version = root.findViewById(R.id.android_version)
         fingerprint = root.findViewById(R.id.fingerprint)
+
         InitUI()
         return root
     }
@@ -75,5 +98,105 @@ class DeviceFragment : Fragment() {
          android_version.text = Device.getVersion()
          fingerprint.text = Device.getFingerprint()
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mAdapter = RssItemsAdapter(this)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = mAdapter
+        swRefresh.setOnRefreshListener(this)
+
+        fetchRss()
+    }
+
+    /**
+     * Fetches Rss Feed Url
+     */
+    private fun fetchRss() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.craft-rom.ml/feed/")
+            .addConverterFactory(RssConverterFactory.create())
+            .build()
+
+        showLoading()
+        val service = retrofit.create(RssService::class.java)
+
+        feedUrl?.apply {
+            service.getRss(this)
+                .enqueue(object : Callback<RssFeed> {
+                    override fun onResponse(call: Call<RssFeed>, response: Response<RssFeed>) {
+                        onRssItemsLoaded(response.body()!!.items!!)
+                        hideLoading()
+                    }
+
+                    override fun onFailure(call: Call<RssFeed>, t: Throwable) {
+                        Toast.makeText(activity, "Failed to fetchRss RSS feed!", Toast.LENGTH_SHORT).show()
+
+                    }
+                })
+        }
+    }
+
+    /**
+     * Loads fetched [RssItem] list to RecyclerView
+     * @param rssItems
+     */
+    fun onRssItemsLoaded(rssItems: List<RssItem>) {
+        mAdapter.setItems(rssItems)
+        mAdapter.notifyDataSetChanged()
+        if (recyclerView.visibility != View.VISIBLE) {
+            recyclerView.visibility = View.VISIBLE
+        }
+    }
+
+    /**
+     * Shows [SwipeRefreshLayout]
+     */
+    private fun showLoading() {
+        swRefresh.isRefreshing = true
+    }
+
+
+    /**
+     * Hides [SwipeRefreshLayout]
+     */
+    fun hideLoading() {
+        swRefresh.isRefreshing = false
+    }
+
+
+    /**
+     * Triggers on [SwipeRefreshLayout] refresh
+     */
+    override fun onRefresh() {
+        fetchRss()
+    }
+
+    /**
+     * Browse [RssItem] url
+     * @param rssItem
+     */
+    override fun onItemSelected(rssItem: RssItem) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(rssItem.link))
+        startActivity(intent)
+    }
+
+    companion object {
+        private const val KEY_FEED = "FEED"
+
+        /**
+         * Creates new instance of [RssFragment]
+         * @param feedUrl Fetched Url Feed
+         * @return Fragment
+         */
+        fun newInstance(feedUrl: String): DeviceFragment {
+            val rssFragment = DeviceFragment()
+            val bundle = Bundle()
+            bundle.putSerializable(KEY_FEED, feedUrl)
+            rssFragment.arguments = bundle
+            return rssFragment
+        }
     }
 }
---
+++ app/src/main/java/com/craftrom/manager/utils/RSS/RssItemsAdapter.kt
@@ -0,0 +1,73 @@
+package com.craftrom.manager.utils.RSS
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.craftrom.manager.R
+import com.craftrom.manager.utils.RSS.converter.RssItem
+import com.squareup.picasso.Picasso
+import java.util.*
+
+
+
+/**
+ * Adapter for listing [RssItem]
+ */
+internal class RssItemsAdapter(private val listener: OnItemClickListener) : RecyclerView.Adapter<RssItemsAdapter.ViewHolder>() {
+
+    private val itemList = ArrayList<RssItem>()
+
+    /**
+     * Set [RssItem] list
+     *
+     * @param items item list
+     */
+    fun setItems(items: List<RssItem>) {
+        itemList.clear()
+        itemList.addAll(items)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.apply {
+            textTitle.text = item.title
+            textPubDate.text = item.publishDate
+
+            if (item.image != null) {
+                Picasso.get()
+                        .load(item.image)
+                        .fit()
+                        .centerCrop()
+                        .into(imageThumb)
+            }
+            itemView.setOnClickListener {
+                listener.onItemSelected(item)
+            }
+
+            itemView.tag = item
+        }
+    }
+
+    override fun getItemCount() = itemList.size
+
+    internal interface OnItemClickListener {
+        fun onItemSelected(rssItem: RssItem)
+    }
+
+    internal class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textTitle: TextView = itemView.findViewById(R.id.tvTitle)
+        val textPubDate: TextView = itemView.findViewById(R.id.tvPubDate)
+        val imageThumb: ImageView = itemView.findViewById(R.id.ivThumb)
+        val llTextContainer: RelativeLayout = itemView.findViewById(R.id.llTextContainer)
+    }
+}
