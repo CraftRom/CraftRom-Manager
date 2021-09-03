742f109cce3300e6a5abdad986f5252deb71c288
Galitsky Dmytro <melles.android@gmail.com>
2021-09-03 21:06:41 +0300

    splash: Delete synthetic

--- app/src/main/java/com/craftrom/manager/splash/SplashActivity.kt
+++ app/src/main/java/com/craftrom/manager/splash/SplashActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.ScaleAnimation
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.craftrom.manager.BuildConfig
@@ -11,13 +13,13 @@ import com.craftrom.manager.MainActivity
 import com.craftrom.manager.R
 import com.craftrom.manager.utils.Constants
 import com.craftrom.manager.utils.Constants.Companion.SPLASH_TIME_OUT
-import kotlinx.android.synthetic.main.activity_splash.*
-import kotlinx.android.synthetic.main.fragment_kernel.*
 import java.util.*
 
 
 class SplashActivity : AppCompatActivity() {
-
+    private lateinit var imageBackground: ImageView
+    private  lateinit var textViewVersion: TextView
+    private  lateinit var textViewSlogan: TextView
 
     private val alphaAnimation by lazy {
         AlphaAnimation(0.0f, 1.0f).apply {
@@ -28,14 +30,14 @@ class SplashActivity : AppCompatActivity() {
 
     private val scaleAnimation by lazy {
         ScaleAnimation(
-                1f,
-                1.25f,
-                1f,
-                1.25f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f
+            1f,
+            1.25f,
+            1f,
+            1.25f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f
         ).apply {
             duration = SPLASH_TIME_OUT
             fillAfter = true
@@ -45,18 +47,20 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        imageBackground = findViewById(R.id.imageBackground)
+        textViewVersion = findViewById(R.id.textViewVersion)
+        textViewSlogan = findViewById(R.id.textViewSlogan)
         initView()
 
     }
 
     private fun initView() {
         val appName = resources.getString(R.string.app_name)
-        textViewVersion.text =
-                resources.getString(R.string.app_name_version, appName, BuildConfig.VERSION_NAME)
+        textViewVersion.text = resources.getString(R.string.app_name_version, appName, BuildConfig.VERSION_NAME)
 
         Glide.with(this)
-                .load(R.drawable.splash)
-                .into(imageBackground)
+            .load(R.drawable.splash)
+            .into(imageBackground)
 
         scaleAnimation.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(animation: Animation?) {
