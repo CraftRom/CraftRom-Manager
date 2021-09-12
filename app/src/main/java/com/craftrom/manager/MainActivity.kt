90dad694f25faa93f5e1bdfad63e88e1f1c70ce9
Galitsky Dmytro <melles.android@gmail.com>
2021-09-12 12:52:44 +0300

    Revert "AdMob"
    This reverts commit 29533fbd7c2a2d377bc8b3e4e958073938b0a1dd.

--- app/src/main/java/com/craftrom/manager/MainActivity.kt
+++ app/src/main/java/com/craftrom/manager/MainActivity.kt
@@ -2,7 +2,6 @@ package com.craftrom.manager
 
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -16,9 +15,7 @@ import androidx.preference.PreferenceManager
 import com.craftrom.manager.activities.IntroActivity
 import com.craftrom.manager.utils.Constants
 import com.craftrom.manager.utils.Device
-import com.google.android.gms.ads.*
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.content_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,27 +23,6 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "CraftRom:MainActivity"
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var mPrefs: SharedPreferences
-    private lateinit var adView: AdView
-
-    private var initialLayoutComplete = false
-    // Determine the screen width (less decorations) to use for the ad width.
-    // If the ad hasn't been laid out, default to the full screen width.
-    private val adSize: AdSize
-        get() {
-            val display = windowManager.defaultDisplay
-            val outMetrics = DisplayMetrics()
-            display.getMetrics(outMetrics)
-
-            val density = outMetrics.density
-
-            var adWidthPixels = ad_view_container.width.toFloat()
-            if (adWidthPixels == 0f) {
-                adWidthPixels = outMetrics.widthPixels.toFloat()
-            }
-
-            val adWidth = (adWidthPixels / density).toInt()
-            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,30 +38,6 @@ class MainActivity : AppCompatActivity() {
         if (!mPrefs.getBoolean(Constants.PREF_SHOW_INTRO, false)) {
             mPrefs.edit().putBoolean(Constants.PREF_SHOW_INTRO, true).apply()
             Constants.changeActivity<IntroActivity>(this)
-        }
-
-        // Initialize the Mobile Ads SDK.
-        MobileAds.initialize(this) { }
-
-        // Set your test devices. Check your logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
-        // to get test ads on this device."
-        MobileAds.setRequestConfiguration(
-            RequestConfiguration.Builder()
-                .setTestDeviceIds(listOf("ABCDEF012345"))
-                .build()
-        )
-
-        adView = AdView(this)
-        ad_view_container.addView(adView)
-        // Since we're loading the banner based on the adContainerView size, we need to wait until this
-        // view is laid out before we can get the width.
-        ad_view_container.viewTreeObserver.addOnGlobalLayoutListener {
-            if (!initialLayoutComplete) {
-                initialLayoutComplete = true
-                loadBanner()
-            }
         }
 
         // Passing each menu ID as a set of Ids because each
@@ -110,40 +62,5 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    private fun loadBanner() {
-        adView.adUnitId = AD_UNIT_ID
-
-        adView.adSize = adSize
-
-        // Create an ad request.
-        val adRequest = AdRequest.Builder().build()
-
-        // Start loading the ad in the background.
-        adView.loadAd(adRequest)
-    }
-
-    companion object {
-        // This is an ad unit ID for a test ad. Replace with your own banner ad unit ID.
-        private const val AD_UNIT_ID = "ca-app-pub-8755738219300967/1454827216"
-    }
-
-    /** Called when leaving the activity  */
-    public override fun onPause() {
-        adView.pause()
-        super.onPause()
-    }
-
-    /** Called when returning to the activity  */
-    public override fun onResume() {
-        super.onResume()
-        adView.resume()
-    }
-
-    /** Called before the activity is destroyed  */
-    public override fun onDestroy() {
-        adView.destroy()
-        super.onDestroy()
     }
 }
