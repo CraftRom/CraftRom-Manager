1d690ac7d441e55a82d199d71bca07577aea56bd
Galitsky Dmytro <melles.android@gmail.com>
2021-09-28 17:01:40 +0300

    Fixed application launch without internet

--- app/src/main/java/com/craftrom/manager/fragments/news/NewsFragment.kt
+++ app/src/main/java/com/craftrom/manager/fragments/news/NewsFragment.kt
@@ -1,10 +1,15 @@
 package com.craftrom.manager.fragments.news
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -48,7 +53,15 @@ class NewsFragment : Fragment(){
             val url = URL(RSS_FEED_LINK)
             rssItems.clear()
             adapter!!.notifyDataSetChanged()
-            RssFeedFetcher(this).execute(url)
+            if(isNetworkAvailable(context) ==true){
+                RssFeedFetcher(this).execute(url)
+                Log.d(Constants.TAG, "NewsFragment: " + RssFeedFetcher(this).execute(url))
+            }
+            else{
+                Toast.makeText(
+                    context, R.string.network_unavailable,
+                    Toast.LENGTH_SHORT).show()
+            }
             swipeContainer.isRefreshing = false
         }
 
@@ -73,10 +86,17 @@ class NewsFragment : Fragment(){
         listV?.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         listV?.adapter = adapter
         adapter!!.notifyDataSetChanged()
+        if(isNetworkAvailable(context) ==true){
+            val url = URL(RSS_FEED_LINK)
+            RssFeedFetcher(this).execute(url)
+            Log.d(Constants.TAG, "NewsFragment: " + RssFeedFetcher(this).execute(url))
+        }
+        else{
+            Toast.makeText(
+                context, R.string.network_unavailable,
+                Toast.LENGTH_SHORT).show()
+        }
 
-        val url = URL(RSS_FEED_LINK)
-        RssFeedFetcher(this).execute(url)
-        Log.d(Constants.TAG, "NewsFragment: " + RssFeedFetcher(this).execute(url))
 
 
     }
@@ -89,6 +109,33 @@ class NewsFragment : Fragment(){
             adapter!!.notifyDataSetChanged()
             swipeContainer.isRefreshing = false
         }
+    }
+
+    fun isNetworkAvailable(context: Context?): Boolean {
+        if (context == null) return false
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
     }
 
 
