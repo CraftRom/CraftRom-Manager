fa0f9d32ea9d4b939440854454c76ecdc91262d7
Galitsky Dmytro <melles.android@gmail.com>
2021-12-07 16:23:44 +0200

    Add curent GPU freq for overlay

--- app/src/main/java/com/craftrom/manager/services/FPS.kt
+++ app/src/main/java/com/craftrom/manager/services/FPS.kt
@@ -19,14 +19,17 @@ import android.widget.TextView
 import com.craftrom.manager.R
 import com.craftrom.manager.utils.Constants
 import com.topjohnwu.superuser.ShellUtils
-import com.topjohnwu.superuser.io.SuFile
 import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 
 class FPS : Service() {
     private var fps = ""
     private var fpsFilePath: String = ""
+    private var gpuFreq = ""
+    private var gpuFreqFilePath: String = ""
     private lateinit var tvFps: TextView
+    private lateinit var tvGpuFreq: TextView
     private lateinit var layoutView: View
     private lateinit var windowManager: WindowManager
 
@@ -37,10 +40,12 @@ class FPS : Service() {
     override fun onCreate() {
         // Attach View To Left Top Corner
         val inflater: LayoutInflater = LayoutInflater.from(this)
-        layoutView = inflater.inflate(R.layout.layout_fps, null)
+        layoutView = inflater.inflate(R.layout.layout_overlay, null)
 
         tvFps = layoutView.findViewById(R.id.tv_fps)
+        tvGpuFreq = layoutView.findViewById(R.id.tv_gpu)
         findFpsFilePath()
+        findGpuFilePath()
 
         val params: WindowManager.LayoutParams =
             WindowManager.LayoutParams(-2, -2, 2038, 4980792, -3)
@@ -63,11 +68,11 @@ class FPS : Service() {
         notificationBuilder.setContentTitle("Craft Rom FPS Meter")
             .setContentText("Keep FPS meter running...").setSmallIcon(R.drawable.ic_new)
         startForeground(69, notificationBuilder.build())
-        onConfigurationChanged(resources.configuration)
 
         // Update FPS Counts
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed(object : Runnable {
+            @SuppressLint("SetTextI18n")
             override fun run() {
                 try {
                     fps = ShellUtils.fastCmd("cat $fpsFilePath")
@@ -75,6 +80,14 @@ class FPS : Service() {
                 } catch (ignored: Exception) {
                     fps = ShellUtils.fastCmd("cat $fpsFilePath").split(" ").toTypedArray()[1]
                     this@FPS.tvFps.text = fps.toDouble().roundToInt().toString()
+                }
+                try {
+                    gpuFreq = ShellUtils.fastCmd("cat $gpuFreqFilePath").toInt().toString()
+                    val gpu = gpuFreq.toInt()
+                    val curGpuFreq = (gpu / 1000000.0).roundToInt()
+                    this@FPS.tvGpuFreq.text = "$curGpuFreq MHz"
+                } catch (ignored:Exception){
+                    this@FPS.tvGpuFreq.text = "error"
                 }
                 handler.postDelayed(this, 1000)
             }
@@ -91,8 +104,26 @@ class FPS : Service() {
                 if (fpsFilePath == "") {
                 fpsFilePath = ShellUtils.fastCmd("find /sys -name fps 2>/dev/null")
                     .trim { it <= ' ' }.split("\n").minOrNull().toString() }
-            Log.e(Constants.TAG, "FPS1 $fpsFilePath")
+            Log.e(Constants.TAG, "\n" +
+                    "FPS file found: $fpsFilePath")
     }
+
+    // Find The GPU File From /sys
+    private fun findGpuFilePath() {
+        gpuFreqFilePath = ShellUtils.fastCmd("find /sys -name gpuclk 2>/dev/null")
+            .trim { it <= ' ' }.split("\n").minOrNull().toString()
+        if (gpuFreqFilePath == "") {
+            gpuFreqFilePath = ShellUtils.fastCmd("find /sys -name frequency 2>/dev/null")
+                .trim { it <= ' ' }.split("\n").minOrNull().toString() }
+        if (gpuFreqFilePath == "") {
+            gpuFreqFilePath = ShellUtils.fastCmd("find /sys -name gpu_rate 2>/dev/null")
+                .trim { it <= ' ' }.split("\n").minOrNull().toString() }
+        if (gpuFreqFilePath == "") {
+            gpuFreqFilePath = ShellUtils.fastCmd("find /sys -name cur_freq 2>/dev/null")
+                .trim { it <= ' ' }.split("\n").minOrNull().toString() }
+        Log.e(Constants.TAG, "GPU file found $gpuFreqFilePath")
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
@@ -101,21 +132,4 @@ class FPS : Service() {
         FPSTile.Service.isRunning = true
         windowManager.removeView(layoutView)
     }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-
-        // Checks the orientation of the screen
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            tvFps.setPadding(
-                resources.getDimension(R.dimen.padding_normal).toInt(),
-                resources.getDimension(R.dimen.padding_normal).toInt(),
-                0,
-                0
-            )
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            tvFps.setPadding(resources.getDimension(R.dimen.padding_normal).toInt(), 0, 0, 0)
-        }
-    }
-
 }
