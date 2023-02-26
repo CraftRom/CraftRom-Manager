2a10a0d614c935d8fc01a2ad79046dee483dc4d0
Galitsky Dmytro <melles.android@gmail.com>
2023-02-26 10:08:54 +0200

    Fix open Telegram link
    When trying to open a Telegram link within the app, if the Telegram app is not installed or does not support opening links via WebView, the user will be redirected to the Telegram installation page on the Google Play app store.

--- app/src/main/java/com/craftrom/manager/ui/about/AboutFragment.kt
+++ app/src/main/java/com/craftrom/manager/ui/about/AboutFragment.kt
@@ -1,25 +1,25 @@
 package com.craftrom.manager.ui.about
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.craftrom.manager.BuildConfig
 import com.craftrom.manager.R
-import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.craftrom.manager.core.ServiceContext
+import com.craftrom.manager.ui.view.ItemWebViewActivity
 
-
-class AboutFragment: Fragment(), View.OnClickListener {
+class AboutFragment : Fragment(), View.OnClickListener {
     private lateinit var imageView: ImageView
     private lateinit var ghimg: ImageView
     private lateinit var webimg: ImageView
-    lateinit var versionApp: TextView
+    private lateinit var versionApp: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,35 +38,54 @@ class AboutFragment: Fragment(), View.OnClickListener {
 
         val versionName = BuildConfig.VERSION_NAME
         versionApp.text = versionName
+
         return root
     }
-    private fun openTG() {
-        val uri = Uri.parse("https://t.me/craftrom")
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        intent.setPackage("org.telegram.messenger.web")
-        startActivity(intent)
-    }
-
-    private fun openGH() {
-        val uri = Uri.parse("https://github.com/CraftRom")
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        intent.setPackage("com.github.android")
-        startActivity(intent)
-    }
-    private fun openWeb() {
-        val uri = Uri.parse("https://www.craft-rom.pp.ua/")
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        startActivity(intent)
-    }
-
 
     override fun onClick(v: View) {
         when (v.id) {
             R.id.tg_link -> openTG()
             R.id.gh_link -> openGH()
             R.id.web_link -> openWeb()
-//            R.id.ll_osl ->  startActivity(Intent(context, OSLActivity::class.java))
         }
     }
 
+    private fun openTG() {
+        val uri = Uri.parse("https://t.me/craftrom")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        intent.setPackage("org.telegram.messenger")
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            // The installed Telegram app doesn't support opening links via WebView, try using org.telegram.messenger.web
+            intent.setPackage("org.telegram.messenger.web")
+            try {
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                // Prompt user to install Telegram or a compatible app
+                val installIntent = Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse("market://details?id=org.telegram.messenger")
+                }
+                startActivity(installIntent)
+            }
+        }
+    }
+
+    private fun openGH() {
+        val uri = Uri.parse("https://github.com/CraftRom")
+        val intent = Intent(ServiceContext.context, ItemWebViewActivity::class.java).apply {
+            putExtra("feedItemUrl", uri)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(intent)
+    }
+
+    private fun openWeb() {
+        val uri = Uri.parse("https://www.craft-rom.pp.ua/")
+        val intent = Intent(ServiceContext.context, ItemWebViewActivity::class.java).apply {
+            putExtra("feedItemUrl", uri)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(intent)
+    }
 }
